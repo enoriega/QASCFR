c48ef2676e57456b37fd3f75bc719ebd168f9dac
Enrique Noriega <enrique.noriega@gmail.com>
2021-06-01 14:52:54 -0700

    Progress

--- qascfr/src/main/scala/org/clulab/exec/NoiseAnalysis.scala
+++ qascfr/src/main/scala/org/clulab/exec/NoiseAnalysis.scala
@@ -5,6 +5,7 @@ import org.clulab.utils.using
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
+import scala.collection.immutable
 import scala.language.implicitConversions
 
 case class NoiseEntry(question:String, answer:String, paths:List[List[String]])
@@ -15,15 +16,16 @@ case class IndividualStats(numHops: Int, numCorrectHops:Int, fact1Correct:Boolea
 }
 case class EntryStats(stats:Seq[IndividualStats]) {
   val containsCorrectPath:Boolean = stats exists (_.isCorrectPath)
-  val numPartiallyCorrect:Int = stats count (_.isPartiallyCorrect)
-  val numTotallyIncorrect:Int = stats.size - numPartiallyCorrect - (if(containsCorrectPath) 1 else 0)
-  val numFact1Correct:Int = stats count (s => s.isPartiallyCorrect && s.fact1Correct)
-  val numFact2Correct:Int = stats count (s => s.isPartiallyCorrect && s.fact2Correct)
+  def numPartiallyCorrect(normalized:Boolean = false):Double = (stats count (_.isPartiallyCorrect)) / (if(normalized) stats.size.toDouble else 1)
+  def numTotallyIncorrect(normalized:Boolean = false):Double = (stats.size - numPartiallyCorrect() - (if(containsCorrectPath) 1 else 0)) / (if(normalized) stats.size.toDouble else 1)
+  def numFact1Correct(normalized:Boolean = false):Double = (stats count (s => s.isPartiallyCorrect && s.fact1Correct))  / (if(normalized) stats.size.toDouble else 1)
+  def numFact2Correct(normalized:Boolean = false):Double = (stats count (s => s.isPartiallyCorrect && s.fact2Correct))  / (if(normalized) stats.size.toDouble else 1)
   val lengths:Seq[Int] = stats map (_.numHops)
-  val numHopsFrequency:Map[Int, Int] = lengths groupBy identity mapValues (_.size)
+  val size:Int = stats.size
+  def numHopsFrequency(normalized:Boolean = false):Map[Int, Double] = lengths groupBy identity mapValues (_.size  / (if(normalized) stats.size.toDouble else 1))
 
-  assert((numFact1Correct + numFact2Correct) == numPartiallyCorrect)
-  assert((numPartiallyCorrect + numTotallyIncorrect + (if(containsCorrectPath) 1 else 0)) == stats.size)
+  assert((numFact1Correct() + numFact2Correct()) == numPartiallyCorrect())
+  assert((numPartiallyCorrect() + numTotallyIncorrect() + (if(containsCorrectPath) 1 else 0)) == stats.size)
 }
 
 object NoiseEntry {
@@ -75,5 +77,24 @@ object NoiseAnalysis extends App {
 
   val allStats =
     relatedEntries map { case (k, v) => crunchExamples(k, v) }
-  var x = 0
+
+  def printStats(stats: Iterable[EntryStats]):Unit = {
+    def meanStd(nums:Iterable[Double], title:String):(Double, Double, Double) = {
+      val mean = nums.sum / nums.size
+      val std = Math.sqrt(nums.map(n => Math.pow(n - mean, 2)).sum / (nums.size - 1))
+      val sem = std / Math.sqrt(nums.size)
+      println(f"$title: Mean: $mean%.5f SE: $sem%.5f Std: $std%.5f")
+      (mean, std, sem)
+    }
+
+
+    println(s"Number of data points: ${stats.size}")
+    meanStd(stats.map(_.size.toDouble), "Number of paths")
+    meanStd(stats.map(_.numPartiallyCorrect(true)), "Percentage of paths partially correct")
+    meanStd(stats.map(_.numHopsFrequency(true).getOrElse(2, 0.0)), "Paths of two hops (Incorrect)")
+  }
+
+
+
+  printStats(allStats)
 }
